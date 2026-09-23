dff99cb6a6f28a208dff45995762073c38d99196
piyushchhakuriya <chhakuriyaji@gmail.com>
2026-09-23 16:12:21 +0530

    Fix URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     const [message, setMessage] = useState("Connecting to backend...");
 
     useEffect(() => {
-        fetch("http://localhost:5000/")
+        fetch("http://65.0.97.177:5000/")
             .then((response) => response.json())
             .then((data) => {
                 setMessage(data.message);
