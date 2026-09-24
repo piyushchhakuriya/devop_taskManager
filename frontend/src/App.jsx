506d7d3b4bcd056595c9a4f271dd03afffb5d201
piyushchhakuriya <chhakuriyaji@gmail.com>
2026-09-24 09:50:00 +0530

    Configure frontend API URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     const [message, setMessage] = useState("Connecting to backend...");
 
     useEffect(() => {
-        fetch("http://65.0.97.177:5000/")
+        fetch(`${import.meta.env.VITE_API_URL}/`)
             .then((response) => response.json())
             .then((data) => {
                 setMessage(data.message);
